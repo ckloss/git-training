dff45030442f58d7501fd29593d561cde9e20d3c
CHEN Xizhong <xchen310@ed.ac.uk>
2016-09-29 08:36:34 +0100

    changes by xizhong chen

--- dem.cpp
+++ dem.cpp
@@ -5,6 +5,8 @@
 //                     GNU GENERAL PUBLIC LICENSE
                    //    Version 3, 29 June 2007
 // Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/> Everyone is permitted to copy and distribute verbatim copies  of this license document, but changing it is not allowed.
+//   The GNU General Public License is a free, copyleft license for
+// software and other kinds of works.
 // For T-MAPPP training event in Lille
 
 
